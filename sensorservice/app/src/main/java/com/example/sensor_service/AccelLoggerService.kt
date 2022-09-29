e42b56e93eece437b842090cb782678595101c60
MMAZeem3 <musa.mazeem@gmail.com>
2022-09-29 15:04:16 -0400

    service

---
+++ sensorservice/app/src/main/java/com/example/sensor_service/AccelLoggerService.kt
@@ -0,0 +1,97 @@
+package com.example.sensor_service
+
+import android.app.*
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.IBinder
+import android.util.Log
+import android.app.NotificationManager
+
+
+class AccelLoggerService: Service(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private lateinit var sensor: Sensor
+    private val NOTIFICATION_CHANNEL_ID = "Channel_1"
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+        // Start foreground service
+        startForeground(1, createNotification())
+
+        return START_STICKY
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        var x = event.values[0]
+        var y = event.values[1]
+        var z = event.values[2]
+        var time = event.timestamp
+        Log.i("service", "time: $time    x: $x     y: $y    z: $z")
+        // var async = SensorEventLoggerTask()
+        // async.execute(event)
+    }
+//    private class SensorEventLoggerTask :AsyncTask<SensorEvent, Unit, Unit>() {
+//        override fun doInBackground(vararg events: SensorEvent) {
+//
+//            for(event in events){
+//                var x = event.values[0]
+//                var y = event.values[1]
+//                var z = event.values[2]
+//                var time = event.timestamp
+//                Log.i("service", "time: $time    x: $x     y: $y    z: $z")
+//            }
+//        }
+//    }
+    private fun createNotification(): Notification {
+        // Create the NotificationChannel
+        val channelName = "foreground_service_channel"
+        val channelDescription = "Notifications for foreground service"
+        val mChannel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            channelName,
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        mChannel.description = channelDescription
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(mChannel)
+
+        // Create Notification
+        val pendingIntent: PendingIntent =
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(
+                    this, 0, notificationIntent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+            }
+
+        return Notification.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("Delta")
+            .setContentText("Accelerometer Recording")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .setTicker("Ticker")
+            .build()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("service", "DESTROYED")
+        sensorManager.unregisterListener(this)
+    }
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // do nothing
+    }
+
+    companion object {
+        private const val DEBUG_TAG = "AccelLoggerService"
+    }
+}
---
+++ sensorservice/app/src/main/java/com/example/sensor_service/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.sensor_service
+
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.hardware.*
+import android.os.Bundle
+import android.util.Log
+import com.example.sensor_service.databinding.ActivityMainBinding
+
+
+class MainActivity : Activity(){
+
+    private lateinit var binding: ActivityMainBinding
+//    private lateinit var mSensorManager: SensorManager
+//    private lateinit var mAccelerometer: Sensor
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // start service
+        val accelIntent = Intent(applicationContext, AccelLoggerService::class.java)
+        startForegroundService(accelIntent)
+
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.i("1", "PAUSED")
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.i("1", "STOPPED")
+
+//    override fun onSensorChanged(p0: SensorEvent) {
+//        val timestamp: Long = p0.timestamp
+//        Log.d("0000",timestamp.toString())
+//    }
+//
+//    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+//    }
+}
